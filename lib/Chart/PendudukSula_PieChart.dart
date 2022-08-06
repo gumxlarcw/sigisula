51916a09dd0d74631b21b6d2c35ed31097c772d3
gumelarcw <wisnucandragumelar@gmail.com>
2022-08-06 23:14:16 +0900

    SYNCING

---
+++ lib/Chart/PendudukSula_PieChart.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:sipadu8203/Data/KepulauanSula_Data.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class KepsulPenduduk extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Container(
+                child: SfCircularChart(series: <CircularSeries>[
+      PieSeries<DataPendudukSula, String>(
+          dataSource: PendudukSula,
+          xValueMapper: (DataPendudukSula data, _) => data.x,
+          yValueMapper: (DataPendudukSula data, _) => data.y,
+          dataLabelMapper: (DataPendudukSula data, _) => data.x,
+          explodeGesture: ActivationMode.singleTap,
+          radius: '60%',
+          // Segments will explode on tap
+          explode: true,
+          explodeOffset: '15%',
+          // First segment will be exploded on initial rendering
+          explodeIndex: 1,
+          dataLabelSettings: DataLabelSettings(
+              isVisible: true,
+              // Avoid labels intersection
+              labelIntersectAction: LabelIntersectAction.shift,
+              labelPosition: ChartDataLabelPosition.outside,
+              connectorLineSettings: ConnectorLineSettings(
+                  type: ConnectorType.curve, length: '40%')))
+    ]))));
+  }
+}
